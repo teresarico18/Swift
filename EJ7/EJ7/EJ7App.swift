e3f2f532b8fe2ff9f5daeedd13eca2b278145383
Teresa Rico <teresa.rico@iest.edu.mx>
2023-10-06 18:30:45 -0600

    Fist commit EJ7

---
+++ EJ7/EJ7/EJ7App.swift
@@ -0,0 +1,17 @@
+//
+//  EJ7App.swift
+//  EJ7
+//
+//  Created by Tere Rico on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct EJ7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
